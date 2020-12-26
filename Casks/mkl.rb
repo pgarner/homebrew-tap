71ad2f55112160589a552ee69d190e69b3bada0a
Phil Garner <pgarner@gmx.ch>
2020-12-26 19:53:40 +0100

    Cask: MKL

---
+++ Casks/mkl.rb
@@ -0,0 +1,7 @@
+cask "mkl" do
+  version "2021.1.0"
+  sha256 "c2fa52d1d6e3cf6e83d2f66b161938e065e6a657153b709d159f32ba5bfbb6e3"
+  url "https://registrationcenter-download.intel.com/akdlm/irc_nas/17426/m_BaseKit_p_2021.1.0.2427_offline.dmg"
+  name "Intel MKL"
+  homepage "https://software.intel.com/content/www/us/en/develop/tools/oneapi/components/onemkl.html"
+end
