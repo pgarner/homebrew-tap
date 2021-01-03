391db50476774eada044b5245d4f802988266241
Phil Garner <pgarner@gmx.ch>
2021-01-03 13:56:08 +0100

    Specify a revision rather than the head / master.zip

--- Formula/sctk.rb
+++ Formula/sctk.rb
@@ -3,8 +3,7 @@ class Sctk < Formula
   homepage "https://www.nist.gov/itl/iad/mig/tools"
   license "NIST"
   version "2.4.11"
-  head "https://github.com/usnistgov/SCTK.git"
-  url "https://github.com/usnistgov/SCTK/archive/master.zip"
+  url "https://github.com/usnistgov/SCTK.git", :revision => "20159b5"
   sha256 "40fd0efb9bc1bf8bb7a899bc7425fc9c3a9cd17cf9b71c0f71cfb210b6c10733"
 
   def install
