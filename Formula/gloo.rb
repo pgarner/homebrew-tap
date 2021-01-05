fa4f2860580825c89b2863b73427ecac03d6e2e0
Phil Garner <pgarner@gmx.ch>
2021-01-05 21:51:20 +0100

    First stab at gloo

---
+++ Formula/gloo.rb
@@ -0,0 +1,26 @@
+class Gloo < Formula
+  desc "Facebook collective communications library"
+  homepage "https://github.com/facebookincubator/gloo"
+  url "https://github.com/facebookincubator/gloo", revision => "1da2117"
+  version "2020.12.03"
+  sha256 ""
+  license "BSD"
+
+  depends_on "cmake" => :build
+  depends_on "cuda@10.2"
+  depends_on "gloo"
+
+  def install
+    args = [
+      "-DUSE_MPI=ON",
+      "-DUSE_CUDA=ON",
+      "-DUSE_NCCL=ON"
+    ]
+    system "cmake", ".", *std_cmake_args, *args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
