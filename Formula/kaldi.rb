69a44a660ad8c30cf672689507f5e9d4c290d718
Phil Garner <pgarner@idiap.ch>
2021-02-16 22:07:37 +0100

    Bring up to date and with cuda 11

--- Formula/kaldi.rb
+++ Formula/kaldi.rb
@@ -1,15 +1,15 @@
 class Kaldi < Formula
   desc "Kaldi ASR"
   homepage "https://kaldi-asr.org/"
-  version "2020.07.06"
-  url "https://github.com/kaldi-asr/kaldi.git", revision: "794732a"
+  version "2021.02.16"
+  url "https://github.com/kaldi-asr/kaldi.git", revision: "d619890"
   head "https://github.com/kaldi-asr/kaldi"
   license "Apache"
 
   depends_on "openfst@1.6.7"
   depends_on "mkl"
   on_linux do
-    depends_on "cuda@10.2"
+    depends_on "cuda"
   end
 
   def install
@@ -26,10 +26,7 @@ class Kaldi < Formula
         # "--shared", # May break an "install"
       ]
       on_linux do
-        # CUDA 10.2 requires gcc-8
-        confargs << "--cudatk-dir=#{Formula["cuda@10.2"].prefix}"
-        ENV["CC"] = "gcc-8"
-        ENV["CXX"] = "g++-8"
+        confargs << "--cudatk-dir=#{Formula["cuda"].prefix}"
       end
       on_macos do
         confargs << "--use-cuda=no"
