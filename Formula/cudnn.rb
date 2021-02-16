71ef28582cefea91cfd13dac8d43acc0671e22cb
Phil Garner <pgarner@gmx.ch>
2021-02-16 21:22:24 +0100

    Use the NVidia dist instead of anaconda

--- Formula/cudnn.rb
+++ Formula/cudnn.rb
@@ -1,16 +1,19 @@
+# See: https://github.com/archlinux/svntogit-community/blob/packages/cudnn/trunk/PKGBUILD
+# or: https://github.com/pytorch/builder/blob/master/common/install_cuda.sh
 class Cudnn < Formula
   desc "NVidia CuDNN Toolkit"
   homepage "https://developer.nvidia.com/cudnn"
   license "NVidia"
-  version "8.0.0"
-  url "https://anaconda.org/nvidia/cudnn/#{version}/download/linux-64/cudnn-#{version}-cuda10.2_0.tar.bz2"
-  sha256 "424c4cd49f049010e08ffc28d0acec538bd6fb9eb8a89ffa4ae1dd3ebee72afa"
+  version "8.1.0"
+  cudaver = "11.2"
+  url "https://developer.download.nvidia.com/compute/redist/cudnn/v#{version}/cudnn-#{cudaver}-linux-x64-v#{version}.77.tgz"
+  sha256 "dbe82faf071d91ba9bcf00480146ad33f462482dfee56caf4479c1b8dabe3ecb"
 
   depends_on :linux
-  depends_on "cuda@10.2"
+  depends_on "cuda"
 
   def install
-    lib.install Dir["lib/*"]
+    lib.install Dir["lib64/*"]
     include.install Dir["include/*"]
   end
 
