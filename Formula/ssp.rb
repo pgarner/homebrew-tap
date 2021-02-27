ab26f8e031e6c8af058a617854efedf1d11106ed
Phil Garner <pgarner@gmx.ch>
2021-02-27 23:05:53 +0100

    Install using pip for the deps

--- Formula/ssp.rb
+++ Formula/ssp.rb
@@ -2,7 +2,7 @@ class Ssp < Formula
   desc "Speech Signal Processing module"
   homepage "https://github.com/idiap/ssp"
   url "https://github.com/idiap/ssp.git", revision: "3949632"
-  version "2018.08.07"
+  version "2018.08"
   license "BSD-3-Clause"
 
   on_macos do
@@ -10,8 +10,11 @@ class Ssp < Formula
   end
 
   def install
-    system "python3", "setup.py", "install", "--prefix=#{prefix}",
-           "--single-version-externally-managed", "--record=installed.txt"
+    # system "python3", "setup.py", "install",
+    # "--prefix=#{prefix}",
+    # "--single-version-externally-managed",
+    # "--record=installed.txt"
+    system "pip3", "-v", "install", "--prefix", "#{prefix}", "."
   end
 
   test do
