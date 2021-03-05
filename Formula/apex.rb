4f510dff1d17a3635d5a83a9111d04d5e30eceb7
Phil Garner <pgarner@idiap.ch>
2021-03-05 16:30:41 +0100

    Add apex pytorch library

---
+++ Formula/apex.rb
@@ -0,0 +1,33 @@
+class Apex < Formula
+  desc "Utilities to streamline mixed precision and distributed training"
+  homepage "https://nvidia.github.io/apex/"
+  url "https://github.com/NVIDIA/apex.git", revision: "e2083df"
+  version "2021.02"
+  license "BSD-3-Clause"
+
+  on_macos do
+    uses_from_macos "python@3"
+  end
+
+  depends_on "cuda"
+  depends_on "pytorch"
+
+  def install
+    # This is not portable; how to get the pythonX.Y ?
+    ENV["PYTHONPATH"] = "#{HOMEBREW_PREFIX}/lib/python3.7/site-packages"
+    args = %W[
+      -v
+      --prefix #{prefix}
+      --disable-pip-version-check
+      --no-cache-dir
+      --global-option=--cpp_ext
+      --global-option=--cuda_ext
+      .
+    ]
+    system "pip3", "install", *args
+  end
+
+  test do
+    system "false"
+  end
+end
