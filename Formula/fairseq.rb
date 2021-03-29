0cba35a75df6bbeafcd329f3a263426e9fb595aa
Phil Garner <pgarner@idiap.ch>
2021-03-29 21:31:18 +0200

    Add fairseq

---
+++ Formula/fairseq.rb
@@ -0,0 +1,29 @@
+class Fairseq < Formula
+  desc "A sequence modeling toolkit"
+  homepage "https://github.com/pytorch/fairseq"
+  version "0.10.2"
+  url "https://github.com/pytorch/fairseq.git", tag: "v#{version}"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "pytorch"
+
+  on_macos do
+    uses_from_macos "python@3"
+  end
+
+  on_linux do
+    depends_on "nccl"
+  end
+
+  def install
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH",
+      "#{prefix}/lib/python#{xy}/site-packages"
+    system "pip3", "-v", "install", "--prefix", "#{prefix}", "."
+  end
+
+  test do
+    system "false"
+  end
+end
