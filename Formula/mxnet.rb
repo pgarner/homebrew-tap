c92c2c6c36e308fbebac6f767f27b6270ab310da
Phil Garner <pgarner@gmx.ch>
2021-05-09 09:26:30 +0200

    Mods to enable comiling on MacOS
    Patches to force python3 are actually because of python2 crashing

--- Formula/mxnet.rb
+++ Formula/mxnet.rb
@@ -13,6 +13,11 @@ class Mxnet < Formula
     depends_on "cudnn11"
     depends_on "nccl"
   end
+  on_macos do
+    uses_from_macos "python@3"
+  end
+
+  patch :DATA
 
   def install
     rm_r "3rdparty/openmp"
@@ -30,6 +35,16 @@ class Mxnet < Formula
         "-DMXNET_CUDA_ARCH=3.7 6.1 7.0 7.5 8.6+PTX"
       ]
     end
+    on_macos do
+      args += [
+        # OpenMP and OpenCV could work, they just have a lot of deps
+        "-DPYTHON_EXECUTABLE=/usr/bin/python3",
+        "-DUSE_CUDA=0",
+        "-DUSE_OPENMP=0",
+        "-DUSE_OPENCV=0"
+      ]
+    end
+
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
@@ -39,3 +54,35 @@ class Mxnet < Formula
     system "false"
   end
 end
+
+__END__
+diff --git a/cpp-package/CMakeLists.txt b/cpp-package/CMakeLists.txt
+index fec86e78e..d3e46200f 100644
+--- a/cpp-package/CMakeLists.txt
++++ b/cpp-package/CMakeLists.txt
+@@ -12,7 +12,7 @@ if(USE_CPP_PACKAGE)
+     MAIN_DEPENDENCY mxnet
+     DEPENDS mxnet ${CMAKE_CURRENT_SOURCE_DIR}/scripts/OpWrapperGenerator.py
+     COMMAND echo "Running: OpWrapperGenerator.py"
+-    COMMAND python OpWrapperGenerator.py $<TARGET_FILE:mxnet>
++    COMMAND python3 OpWrapperGenerator.py $<TARGET_FILE:mxnet>
+     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/scripts
+   )
+ 
+diff --git a/cpp-package/cpp-package.mk b/cpp-package/cpp-package.mk
+index b9e7c3331..f9d83c423 100644
+--- a/cpp-package/cpp-package.mk
++++ b/cpp-package/cpp-package.mk
+@@ -36,10 +36,10 @@ cpp-package-clean:
+ 	rm -f $(CPP_PACKAGE_OP_H_FILE)
+ 
+ $(CPP_PACKAGE_OP_H_FILE): lib/libmxnet.so cpp-package/scripts/OpWrapperGenerator.py
+-	(cd cpp-package/scripts; python OpWrapperGenerator.py $(ROOTDIR)/lib/libmxnet.so)
++	(cd cpp-package/scripts; python3 OpWrapperGenerator.py $(ROOTDIR)/lib/libmxnet.so)
+ 
+ cpp-package-lint:
+-	(cd cpp-package; python scripts/lint.py dmlc ${LINT_LANG} include example)
++	(cd cpp-package; python3 scripts/lint.py dmlc ${LINT_LANG} include example)
+ 
+ include cpp-package/example/example.mk
+ include cpp-package/example/inference/inference.mk
