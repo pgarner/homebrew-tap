d8b595b1ef7ce2e1b72b6b553ab4a12d25a48230
Phil Garner <pgarner@gmx.ch>
2021-07-11 12:09:41 +0200

    Add autotools on MacOS
    ...although I think it still doesn't work

--- Formula/mitlm.rb
+++ Formula/mitlm.rb
@@ -5,6 +5,12 @@ class Mitlm < Formula
   url "https://github.com/mitlm/mitlm.git", revision: "553edca"
   license "MIT"
 
+  on_macos do
+    depends_on "libtool"
+    depends_on "automake"
+    depends_on "autoconf"
+  end
+
   def install
     system "autoreconf", "-i"
     system "./configure", "--disable-debug",
