9b2a6f448685c8c3deecfb4dc7cadfce89f92af4
Phil Garner <pgarner@gmx.ch>
2021-11-11 21:21:32 +0100

    Add a virtualenv; confie the include thing to linux

--- Formula/sys-python.rb
+++ Formula/sys-python.rb
@@ -1,21 +1,24 @@
-#
-# This is a *horrible* hack
-# Brew's shims don't allow include paths into /usr/include. This means you
-# can't use some system files, in this case /usr/include/pythonX.Y/*
-# So, fool it into thinking it's a local install.
-#
 class SysPython < Formula
-  desc "Links to cause homebrew to use the system's python"
+  include Language::Python::Virtualenv
+
+  desc "Cause homebrew to use the system's python3"
   homepage "https://github.com/pgarner/homebrew-tap"
-  version "2021.03"
-  url "https://github.com/pgarner/homebrew-tap.git", revision: "bff5a40"
+  version "2021.11"
+  url "https://github.com/pgarner/homebrew-tap.git", tag: "dummy"
   license "BSD-3-Clause"
 
-  keg_only "it's a horrible hack"
-
   def install
-    xy = Language::Python.major_minor_version("python3")
-    include.install_symlink Dir["/usr/include/python#{xy}/*"]
+    # This is a perfectly reasonable hack
+    virtualenv_create(prefix, "python3")
+
+    on_linux do
+      # This is a *horrible* hack
+      # Brew's shims don't allow include paths into /usr/include. This means
+      # you can't use some system files, in this case /usr/include/pythonX.Y/*
+      # So, fool it into thinking it's a local install.
+      xy = Language::Python.major_minor_version("python3")
+      include.install_symlink "/usr/include/python#{xy}"
+    end
   end
 
   test do
