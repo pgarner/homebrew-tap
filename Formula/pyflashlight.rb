f2690f78e0c86b629590fcbc47a7ee91e3cb33f4
Phil Garner <pgarner@gmx.ch>
2021-11-21 18:17:27 +0100

    Version up

--- Formula/pyflashlight.rb
+++ Formula/pyflashlight.rb
@@ -1,31 +1,22 @@
 class Pyflashlight < Formula
   desc "Python bindings for Flashlight"
   homepage "https://github.com/facebookresearch/flashlight/tree/master/bindings/python"
-  version "0.3"
+  version "0.3.1"
   url "https://github.com/facebookresearch/flashlight.git", tag: "v#{version}"
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
   depends_on "mkl"
   depends_on "kenlm"
-
-  on_macos do
-    uses_from_macos "python@3"
-  end
+  depends_on "pytorch"
 
   on_linux do
-    depends_on "cuda11"
-    depends_on "sys-python"
+    depends_on "cuda"
   end
 
   def install
-    # The things that pip installs as deps
-    xy = Language::Python.major_minor_version("python3")
-    ENV.prepend_create_path "PYTHONPATH",
-      "#{prefix}/lib/python#{xy}/site-packages"
     ENV["VERBOSE"] = "1"
     cd buildpath/"bindings/python" do
-      #system "pip3", "-v", "install", "--prefix", "#{prefix}", "-e", "."
       system "python3", *Language::Python.setup_install_args(prefix)
     end
   end
