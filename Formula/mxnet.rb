13dc9f7ec96096673e2247af00de6b9ba8e52bf2
Phil Garner <pgarner@gmx.ch>
2021-12-27 15:33:46 +0100

    Update to 1.9.0, but I could not get it working

--- Formula/mxnet.rb
+++ Formula/mxnet.rb
@@ -1,7 +1,7 @@
 class Mxnet < Formula
   desc "Lightweight, Portable, Flexible Distributed/Mobile Deep Learning"
   homepage "https://mxnet.apache.org/"
-  version "1.7.0"
+  version "1.9.0"
   url "https://github.com/apache/incubator-mxnet.git", tag: "#{version}"
   head "https://github.com/apache/incubator-mxnet"
   license "Apache"
@@ -9,23 +9,18 @@ class Mxnet < Formula
   depends_on "cmake" => :build
   depends_on "mkl"
   on_linux do
-    depends_on "cuda11"
-    depends_on "cudnn11"
+    depends_on "cuda"
+    depends_on "cudnn"
     depends_on "nccl"
   end
-  on_macos do
-    uses_from_macos "python@3"
-  end
-
-  patch :DATA
 
   def install
     rm_r "3rdparty/openmp"
     rm_r "3rdparty/nvidia_cub"
     args = [
       "-DUSE_MKLDNN=1",
-      "-DUSE_CPP_PACKAGE=1", # gone in 2.0.0
-      "-DINSTALL_EXAMPLES=1"
+      "-DUSE_CPP_PACKAGE=1",
+      "-DBUILD_CPP_EXAMPLES=0"
     ]
     on_linux do
       args += [
@@ -54,35 +49,3 @@ class Mxnet < Formula
     system "false"
   end
 end
-
-__END__
-diff --git a/cpp-package/CMakeLists.txt b/cpp-package/CMakeLists.txt
-index fec86e78e..d3e46200f 100644
---- a/cpp-package/CMakeLists.txt
-+++ b/cpp-package/CMakeLists.txt
-@@ -12,7 +12,7 @@ if(USE_CPP_PACKAGE)
-     MAIN_DEPENDENCY mxnet
-     DEPENDS mxnet ${CMAKE_CURRENT_SOURCE_DIR}/scripts/OpWrapperGenerator.py
-     COMMAND echo "Running: OpWrapperGenerator.py"
--    COMMAND python OpWrapperGenerator.py $<TARGET_FILE:mxnet>
-+    COMMAND python3 OpWrapperGenerator.py $<TARGET_FILE:mxnet>
-     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/scripts
-   )
- 
-diff --git a/cpp-package/cpp-package.mk b/cpp-package/cpp-package.mk
-index b9e7c3331..f9d83c423 100644
---- a/cpp-package/cpp-package.mk
-+++ b/cpp-package/cpp-package.mk
-@@ -36,10 +36,10 @@ cpp-package-clean:
- 	rm -f $(CPP_PACKAGE_OP_H_FILE)
- 
- $(CPP_PACKAGE_OP_H_FILE): lib/libmxnet.so cpp-package/scripts/OpWrapperGenerator.py
--	(cd cpp-package/scripts; python OpWrapperGenerator.py $(ROOTDIR)/lib/libmxnet.so)
-+	(cd cpp-package/scripts; python3 OpWrapperGenerator.py $(ROOTDIR)/lib/libmxnet.so)
- 
- cpp-package-lint:
--	(cd cpp-package; python scripts/lint.py dmlc ${LINT_LANG} include example)
-+	(cd cpp-package; python3 scripts/lint.py dmlc ${LINT_LANG} include example)
- 
- include cpp-package/example/example.mk
- include cpp-package/example/inference/inference.mk
