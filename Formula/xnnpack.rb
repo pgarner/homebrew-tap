70ac59c3f6b3f9a847a084587a020a21892a0119
Phil Garner <pgarner@idiap.ch>
2021-12-28 16:46:34 +0100

    Prevent xnnpack from installing googletest

--- Formula/xnnpack.rb
+++ Formula/xnnpack.rb
@@ -9,7 +9,10 @@ class Xnnpack < Formula
 
   def install
     args = [
-      "-DCMAKE_POSITION_INDEPENDENT_CODE=1"
+      # The last two prevent it *installing* googletest and clashing
+      "-DCMAKE_POSITION_INDEPENDENT_CODE=1",
+      "-DXNNPACK_BUILD_TESTS=0",
+      "-DXNNPACK_BUILD_BENCHMARKS=0"
     ]
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
     system "cmake", "--build", "build"
