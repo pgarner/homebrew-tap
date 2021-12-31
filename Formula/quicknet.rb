fef96db7166f38d5a76423537b9a46bdf2e7b09a
Phil Garner <pgarner@gmx.ch>
2021-12-31 18:15:50 +0100

    Convert the version to a number to keep brew happier
    Otherwise it tried to upgrade every time; presumably can't calculate a numerical version.

--- Formula/quicknet.rb
+++ Formula/quicknet.rb
@@ -1,8 +1,8 @@
 class Quicknet < Formula
   desc "ICSI's MLP implementation"
   homepage "http://www1.icsi.berkeley.edu/Speech/qn.html"
-  version "v3_33"
-  url "ftp://ftp.icsi.berkeley.edu/pub/real/davidj/quicknet-#{version}.tar.gz"
+  version "3.33"
+  url "ftp://ftp.icsi.berkeley.edu/pub/real/davidj/quicknet-v#{version.major}_#{version.minor}.tar.gz"
   sha256 "035003767e1d7580ae30bdf3c4ff839a9b380e239d4b2ecde85df55d94f9a145"
   license "BSD (probably)"
 
