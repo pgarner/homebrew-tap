cc48cb935fb0cd0f7a06f14fcb6f736d15999668
Phil Garner <pgarner@gmx.ch>
2022-01-30 13:26:50 +0100

    Add libresample from core but with -fPIE

---
+++ Formula/libresample.rb
@@ -0,0 +1,16 @@
+class Libresample < Formula
+  desc "Audio resampling C library"
+  homepage "https://ccrma.stanford.edu/~jos/resample/Available_Software.html"
+  url "https://deb.debian.org/debian/pool/main/libr/libresample/libresample_0.1.3.orig.tar.gz"
+  sha256 "20222a84e3b4246c36b8a0b74834bb5674026ffdb8b9093a76aaf01560ad4815"
+  license "LGPL-2.1"
+
+  def install
+    ENV.append "CFLAGS", "-fPIE"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    lib.install "libresample.a"
+    include.install "include/libresample.h"
+  end
+end
