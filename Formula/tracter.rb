6c2a3c0ca29209155b12a98ddf8da42d23bb57cc
Phil Garner <pgarner@gmx.ch>
2022-04-10 20:02:23 +0200

    Depend on our libresample

--- Formula/tracter.rb
+++ Formula/tracter.rb
@@ -1,13 +1,13 @@
 class Tracter < Formula
   desc "A lightweight dataflow framework"
   homepage "https://github.com/idiap/tracter"
-  url "https://github.com/idiap/tracter/archive/v2.0.tar.gz"
   version "2.0"
+  url "https://github.com/idiap/tracter/archive/v#{version}.tar.gz"
   sha256 "e2b53e13f93011ed7d10e4333ebb65ff4d6c065e2eea53f46abace5e7e51abee"
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
-  depends_on "libresample"
+  depends_on "pgarner/tap/libresample"
   depends_on "lube"
   depends_on "sptk"
   depends_on "libssp"
