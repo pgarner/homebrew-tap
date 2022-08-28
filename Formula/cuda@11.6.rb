f2c2b93d91a094ea7072e0f9b2ddda8f0b11651d
Phil Garner <philipgarner@alumni.soton.ac.uk>
2022-08-28 11:24:08 +0200

    Add CUDA 11.6 and CUDNN 8.4 by replacing the useless 11.2 version

--- Formula/cuda@11.6.rb
+++ Formula/cuda@11.6.rb
@@ -1,17 +1,17 @@
-class CudaAT112 < Formula
+class CudaAT116 < Formula
   desc "NVidia CUDA Toolkit"
   homepage "https://developer.nvidia.com/cuda-zone"
   license "NVidia"
-  version "11.2.0"
-  vdriver = "460.27.04"
+  version "11.6.0"
+  vdriver = "510.39.01"
   url "https://developer.download.nvidia.com/compute/cuda/#{version}/local_installers/cuda_#{version}_#{vdriver}_linux.run"
-  sha256 "9c50283241ac325d3085289ed9b9c170531369de41165ce271352d4a898cbdce"
+  sha256 "1783da6d63970786040980b57fa3cb6420142159fc7d0e66f8f05c4905d98c83"
 
   keg_only :versioned_formula
   depends_on :linux
 
   def install
-    vdriver = "460.27.04" # Why does it get lost?
+    vdriver = "510.39.01" # Why does it get lost?
     system "sh", "cuda_#{version}_#{vdriver}_linux.run",
            "--installpath=#{prefix}",
            "--silent", "--override", "--toolkit",
--- Formula/cudnn@8.4.rb
+++ Formula/cudnn@8.4.rb
@@ -1,20 +1,20 @@
 # See: https://github.com/archlinux/svntogit-community/blob/packages/cudnn/trunk/PKGBUILD
 # or: https://github.com/pytorch/builder/blob/master/common/install_cuda.sh
-class CudnnAT81 < Formula
+class CudnnAT84 < Formula
   desc "NVidia CuDNN Toolkit"
   homepage "https://developer.nvidia.com/cudnn"
   license "NVidia"
-  version "8.1.0"
-  cudaver = "11.2"
-  url "https://developer.download.nvidia.com/compute/redist/cudnn/v#{version}/cudnn-#{cudaver}-linux-x64-v#{version}.77.tgz"
-  sha256 "dbe82faf071d91ba9bcf00480146ad33f462482dfee56caf4479c1b8dabe3ecb"
+  version "8.4.1"
+  cudaver = "11.6"
+  url "https://developer.download.nvidia.com/compute/redist/cudnn/v#{version}/local_installers/#{cudaver}/cudnn-linux-x86_64-#{version}.50_cuda#{cudaver}-archive.tar.xz"
+  sha256 "ec96d2376d81fca42bdd3d4c3d705a99b29a065bab57f920561c763e29c67d01"
 
   keg_only :versioned_formula
   depends_on :linux
-  depends_on "cuda@11.2"
+  depends_on "cuda@11.6"
 
   def install
-    lib.install Dir["lib64/*"]
+    lib.install Dir["lib/*"]
     include.install Dir["include/*"]
   end
 
